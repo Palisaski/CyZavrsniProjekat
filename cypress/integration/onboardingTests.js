467909d6e28b5abd62f814218a9e877ce3721928
Palisaski <palisaskim@gmail.com>
2022-04-20 12:14:08 +0200

    User login

--- cypress/integration/onboardingTests.js
+++ cypress/integration/onboardingTests.js
@@ -3,10 +3,12 @@ describe('First Test Suite', function() {
       cy.visit('/')
   });
   
-  it('Log in test', function(){
+  it('Log in as a user', function(){
     cy.get(".theme-btn").contains("Login").click();
+    cy.get("input[placeholder='Email']").click({ force: true }).type('user@phptravels.com');
+    cy.get("input[placeholder='Password']").click({ force: true }).type('demouser');
+    cy.get("button[type=submit]").contains("Login").click({ force: true });
+    
     }
-    //cy.loginUser('user@phptravels.com', 'demouser')
-  
-  
+    
   )})
