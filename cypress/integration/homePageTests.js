1c1c576e7c899a691b9a134b1572a414dc2fd852
Palisaski <palisaskim@gmail.com>
2022-04-29 14:25:42 +0200

    Adding 2. Test

---
+++ cypress/integration/homePageTests.js
@@ -0,0 +1,23 @@
+describe('Home page features', function() {
+  beforeEach('Go to application', function(){
+    cy.visit('/')
+})});
+
+  it.only('1. Search', function() {
+    cy.visit('/')
+    cy.get('#select2-hotels_city-container').click({force : true})
+    cy.get('.select2-search__field').type('Madrid',{force : true}).then( () => {
+      cy.wait(3000)
+      cy.get('.select2-results__option').contains('Madrid,Spain').click()
+    })
+    cy.get('.guest_hotels').click({force : true})
+    cy.get('#nationality').select('Tuvalu', {force : true})
+    cy.get('button[type=submit]').contains('Search').click({force : true})
+
+  
+  it('2. Feature tours - Looking through photo gallery', function() {
+    cy.visit('/')
+    cy.get('.card-item').contains('Sydney and Bondi Beach Explorer').click({force : true})
+    cy.get('.theme-btn').contains('More Photos').click({force : true})
+    cy.get('.fancybox-button--arrow_right').click()
+  })})
