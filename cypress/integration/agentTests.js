7cf175f47bd6a9a22dd73547ec67eefe4f9dd925
Palisaski <palisaskim@gmail.com>
2022-05-02 12:40:34 +0200

    Adding 2.Add Funds to  AgentTests

--- cypress/integration/agentTests.js
+++ cypress/integration/agentTests.js
@@ -5,8 +5,14 @@ describe('Home page features', function() {
     cy.visit('/')
 })});
 
-it('1. ', function() {
+it('1. Add fund', function() {
+  cy.visit('/');
+  cy.get('.theme-btn').contains('Login').click();
   cy.login(agent);
-  cy.get('.sidebar-menu > list-items').contains('Add Funds')
+  cy.get('button[type=submit]').contains('Login').click({ force: true });
+  cy.get('.sidebar-menu-wrap').contains('Add Funds').click();
+  cy.get('#gateway_paypal').click()
+  cy.get('input[name=price]').clear().type('100')
+  cy.get('button[type=submit]').contains('Pay Now').click()
 
 } )
